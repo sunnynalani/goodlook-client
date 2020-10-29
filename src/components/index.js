0985308ce8ade41f5f0ab2c5fb63d2c785acf71f
jjchung3 <joejoonchung@gmail.com>
2020-10-29 04:08:57 -0700

    chore(): minor ui change test...

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 // export { default as Header } from './Header'
 // export { default as Icon } from './Icon'
+export { default as Button } from './Button'
 export { default as MainNavigator } from './MainNavigator'
 export { default as Text } from './Text'
 export { default as ThemeProvider } from './ThemeProvider'
--- src/screens/SignInView/styles.js
+++ src/screens/SignInView/styles.js
@@ -4,7 +4,7 @@ import { normalize } from '../../utils'
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1B1B1B',
+    backgroundColor: '#linear-gradient(#ece9e6, #ffffff)',
     height: '100%',
     width: '100%',
   },
@@ -22,33 +22,48 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   titleText: {
-    color: '#FFFFF0',
-    fontSize: normalize(36),
+    color: '#000',
+    fontSize: normalize(48),
     textAlign: 'center',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 2,
   },
   loginText: {
     textDecorationLine: 'underline',
     fontSize: normalize(24),
     textAlign: 'center',
-    color: '#FFFFF0',
   },
   plainSubtext: {
     fontSize: normalize(18),
     textAlign: 'center',
-    color: '#FFFFF0',
+    color: '#000',
   },
   signUpText: {
     textDecorationLine: 'underline',
     fontSize: normalize(18),
     textAlign: 'center',
     textShadowRadius: 1,
-    color: '#FFFFF0',
+    color: '#000',
   },
   guestText: {
     textDecorationLine: 'underline',
     fontSize: normalize(14),
     textAlign: 'center',
-    color: '#FFFFF0',
+    color: '#000',
+  },
+  lineStyle: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    shadowColor: '#000000',
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 1,
+    },
+    alignSelf: 'center',
+    width: '60%',
   },
 })
 
