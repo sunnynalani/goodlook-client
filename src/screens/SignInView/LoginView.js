16768bcecd14fad6b629e8cd06fe6b2a96a7c5da
Jaime Leal <jaimito.leal@hotmail.com>
2020-10-30 12:58:49 -0700

    added email and psswd form, log in button, forgot psswd option

--- src/screens/SignInView/LoginView.js
+++ src/screens/SignInView/LoginView.js
@@ -1,13 +1,55 @@
 import React from 'react'
-import { Text, View } from '../../components'
+import { Button, TextInput } from 'react-native-paper'
+import { Text, View, TouchableOpacity } from '../../components'
 import styles from './styles'
 
 /**
  *
  * MAKE LOGIN UP VIEW HERE
  */
+
 const LoginView = ({ navigation }) => {
-  return <View></View>
+  const logInButton = () => {
+    navigation.navigate('Log In')
+  }
+
+  const toForgotPswd = () => {
+    navigation.navigate('ForgotPassword')
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleTextContainer}>
+        <TextInput label="Email" style={{ margin: 16 }} />
+        {/* <br></br> */}
+        <TextInput
+          secureTextEntry={true}
+          label="Password"
+          style={{ margin: 16 }}
+        />
+        {/* <br></br>   */}
+        <View>
+          <Button
+            mode="contained"
+            onPress={() => console.log('Pressed')}
+            accessibilityLabel="Log In button"
+            theme={{ roundness: 10 }}
+            style={{ width: 150, margin: 16 }}
+          >
+            <View>
+              <Text style={styles.lgText}>Log In</Text>
+            </View>
+          </Button>
+        </View>
+      </View>
+      <View style={styles.subtitleContainer}>
+        <View>
+          <TouchableOpacity onPress={toForgotPswd}>
+            <Text style={styles.guestText}>Forgot Password?</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
 }
 
 export default LoginView
